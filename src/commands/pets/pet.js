f84cb1d9ce0c5dcf23f6e0fd716bfa4d9e6c29e5
Joaquim <joaquimmy@yahoo.com>
2020-02-19 15:45:39 -0300

    Move pet top to different file

--- src/commands/pets/pet.js
+++ src/commands/pets/pet.js
@@ -21,6 +21,7 @@ const {
 const flatSeconds = require('../../util/flatSeconds');
 const xp = require('../../util/magicformula');
 
+const topPets = require('./_topPets');
 const listPets = require('./_listPets');
 
 const messages = {
@@ -372,36 +373,7 @@ module.exports = new Command({
     }
 
     if (args[0] && ['top', 'rank', 'ranking'].includes(args[0].toLowerCase())) {
-      const top5Pets = await Pet.find({})
-        .limit(5)
-        .sort({ level: -1 })
-        .exec();
-
-      top5Pets.sort((a, b) => {
-        return a.level === b.level ? b.experience - a.experience : b.level - a.level;
-      });
-
-      const top5 = top5Pets.map((p, i) => {
-        const medals = {
-          '0': ':first_place:',
-          '1': ':second_place:',
-          '2': ':third_place:',
-          '3': ':small_blue_diamond:',
-          '4': ':small_blue_diamond:',
-        };
-        // if cant find user they don't share a server with bloo.
-        // TODO: Check profiles database for this user.
-        const u = this.client.users.get(p.owner);
-        return `${medals['' + i]}**${p.name} (${u ? u.tag : 'Anonymous Bloo Baby'}):** L:${
-          p.level
-        } XP:${p.experience}`;
-      });
-
-      const msg = [];
-      msg.push(messages.TOP_HEADING);
-      top5.forEach(top5Pet => msg.push(top5Pet));
-
-      this.send(...msg, { split: true });
+      topPets(pets, message, args, call, messages);
       return;
     }
 
