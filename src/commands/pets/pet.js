3b07f8f40ddd13a7101f09a75215c50d81f3c6a8
Joaquim <joaquimmy@yahoo.com>
2020-02-19 15:39:01 -0300

    Grouped all pet messages together

--- src/commands/pets/pet.js
+++ src/commands/pets/pet.js
@@ -21,6 +21,55 @@ const {
 const flatSeconds = require('../../util/flatSeconds');
 const xp = require('../../util/magicformula');
 
+const listPets = require('./_listPets');
+
+const messages = {
+  // General
+  GENERIC_OKAY: 'Okay then.',
+  FANCY_OKAY: 'Okie Dokie Artichokie.',
+  SILLY_OKAY: 'Alrighty then.',
+  SOMETHING_WENT_WRONG: 'Something went wrong. :c',
+  // Adoption
+  ADOPT_ARE_YOU_SURE: `Are you sure? That will cost you **${PET_PRICE}${INK_EMOJI}**`,
+  ADOPT_NOT_ENOUGH_BLOO_INK: `You don't have enough Bloo Ink${INK_EMOJI} to adopt a pet. :c`,
+  ADOPT_CANT_ADOPT_MORE_PETS: "You can't adopt any more pets. :c",
+  ADOPT_SUCCESS: `Congratulations, {0}!\nYou just adopted an adorable pet named **{1}**.`,
+  // Abandoning
+  ABANDON_WHICH_PET: format(
+    'Which pet would you like to throw away forever? **{0}**?',
+    "I can't believe I have to be the bearer of bad news and tell them their master doesn't love them anymore.",
+    "Please don't do this :frowning:",
+  ),
+  ABANDON_ARE_YOU_SURE:
+    "Are you sure you would like to do this? There isn't a possibility of getting the same pet back. And you may hurt their *feelings* :c \n**Please confirm that you would like to do this.**",
+  ABANDON_PET_ABANDONED:
+    'You have gotten rid of your pet **{0}**. \n \nI bet you feel bad now.. *How could you?* :frowning:',
+  ABANDON_YOU_HAVE_NO_PETS:
+    "I hate to see this... You really don't like pets to the point... That you tried... To get rid.. Of a pet, that __***doesn't exist***__.",
+  ABANDON_PET_NOT_FOUND:
+    "I do not see a pet under that name, maybe see if you are spelling it correctly? Or.... maybe you shouldn't get rid of your pet :smile: \nBut if you are serious about wanting to abandon your pet, please check your spelling and try again!",
+  ABANDON_WILL_NOT_ABANDON_PET:
+    '**Phew.** Thank goodness you decided not to! Thank you for having a heart.',
+  // Patting
+  PAT_PATTED_THE_PET: 'You have pat your pet **{0}** for **{1}** exp!',
+  PAT_COOLDOWN: `You can pat your pets every **${PET_PAT_COOLDOWN}** minutes.`,
+  // Renaming
+  RENAME_NO_PETS: "You don't have a pet to rename.",
+  RENAME_NO_MONEY:
+    "Renaming costs {0}. You do not have enough funds to rename your pet. Please consider voting for me (**!b vote**) to gather more Blue Ink, or use **!b daily** if you haven't already!",
+  RENAME_WHICH_PET: 'Which pet would you like to rename? **{0}**?',
+  RENAME_PET_NOT_FOUND: 'I do not see a pet under this name; did you mean **{0}**?',
+  RENAME_ARE_YOU_SURE: 'Are you sure you would like to rename your pet? That will cost **{0}**.',
+  RENAME_ASK_NEW_NAME: "What would you like your pet's new name to be?",
+  RENAME_NAME_HAS_PROFANITY: `I'm going to need you to reconsider that name. That is an explicit name that I cannot allow you to name your pet that.`,
+  RENAME_NAME_HAS_SYMBOLS: 'Only characters A-Z are allowed in your pets name.',
+  RENAME_NAME_TOO_LONG: `That name is too long. Pet names can only be ${PET_MAX_NAME_LENGTH} or less.`,
+  RENAME_BALANCE_CHANGED_DURING_RENAMING: 'You do not have enough Bloo ink to rename your pet.',
+  RENAME_SUCCESS: `Your pet is now named {0} and you have {1} Bloo Ink.`,
+  // Top
+  TOP_HEADING: '__Currently these are the best pets!.__',
+};
+
 module.exports = new Command({
   name: 'pet',
   description: Text.lines(
@@ -29,6 +78,7 @@ module.exports = new Command({
     'Use **pet pat** to pat your pets.',
     'Use **pet rename** to rename your pets.',
     'Use **pet abandon** to get rid of a pet.',
+    'Use **pet top** to see the top pets.',
     'You can have a max of 2 pets.',
   ),
   category: 'pets',
@@ -40,23 +90,23 @@ module.exports = new Command({
     if (args[0] && ['a', 'adopt'].includes(args[0].toLowerCase())) {
       const response = await Prompter.confirm({
         channel: message.channel,
-        question: `Are you sure? That will cost you **${PET_PRICE}${INK_EMOJI}**`,
+        question: messages.ADOPT_ARE_YOU_SURE,
         userId: call.caller,
       });
       if (response !== true) {
-        this.send('Okay then.');
+        this.send(messages.GENERIC_OKAY);
         return;
       }
 
       // monie check
       if (call.profile.money < PET_PRICE) {
-        this.send(`You don't have enough Bloo Ink${INK_EMOJI} to adopt a pet. :c`);
+        this.send(messages.ADOPT_NOT_ENOUGH_BLOO_INK);
         return;
       }
 
       // pet count check
       if (pets.length >= MAX_PET_COUNT) {
-        this.send("You can't adopt any more pets. :c");
+        this.send(messages.ADOPT_CANT_ADOPT_MORE_PETS);
         return;
       }
 
@@ -80,7 +130,10 @@ module.exports = new Command({
       // response
       const embed = new MessageEmbed({
         title: 'Pet adopted!',
-        description: `Congratulations, ${message.author}!\nYou just adopted an adorable pet named **${petName}**.`,
+        description: messages.ADOPT_SUCCESS.replace(/\{0\}/, message.author).replace(
+          /\{1\}/,
+          petName,
+        ),
         files: [{ name: 'pet.png', attachment: petImage }],
         thumbnail: { url: 'attachment://pet.png' },
         color: 0x009900,
@@ -91,24 +144,9 @@ module.exports = new Command({
 
     // Arg === abandon/yeet/delete
     if (args[0] && ['abandon', 'yeet', 'delete'].includes(args[0].toLowerCase())) {
-      // abandon pet
-      const whichPetToAbandonMessage = format(
-        'Which pet would you like to throw away forever? **{0}**?',
-        "I can't believe I have to be the bearer of bad news and tell them their master doesn't love them anymore.",
-        "Please don't do this :frowning:",
-      );
-      const areYouSureMessage =
-        "Are you sure you would like to do this? There isn't a possibility of getting the same pet back. And you may hurt their *feelings* :c \n**Please confirm that you would like to do this.**";
-      const petAbandonedMessage =
-        'You have gotten rid of your pet **{0}**. \n \nI bet you feel bad now.. *How could you?* :frowning:'; // << undefined
-      const youHaveNoPetsMessage =
-        "I hate to see this... You really don't like pets to the point... That you tried... To get rid.. Of a pet, that __***doesn't exist***__.";
-      const userEnteredAPetNameThatTheyDontOwnMessage =
-        "I do not see a pet under that name, maybe see if you are spelling it correctly? Or.... maybe you shouldn't get rid of your pet :smile: \nBut if you are serious about wanting to abandon your pet, please check your spelling and try again!";
-
       // 0 pets
       if (pets.length < 1) {
-        this.send(youHaveNoPetsMessage);
+        this.send(messages.ABANDON_YOU_HAVE_NO_PETS);
         return;
       }
 
@@ -118,7 +156,7 @@ module.exports = new Command({
       if (pets.length > 1) {
         const res = await Prompter.message({
           channel: message.channel,
-          question: whichPetToAbandonMessage.replace(/\{0\}/g, pets.map(p => p.name).join(', ')),
+          question: messages.ABANDON_WHICH_PET.replace(/\{0\}/g, pets.map(p => p.name).join(', ')),
           userId: call.caller,
           max: 1,
           deleteMessage: false,
@@ -133,7 +171,7 @@ module.exports = new Command({
         }
 
         if (!petToDelete) {
-          this.send(userEnteredAPetNameThatTheyDontOwnMessage);
+          this.send(messages.ABANDON_PET_NOT_FOUND);
           return;
         }
         // only 1 pet
@@ -143,26 +181,25 @@ module.exports = new Command({
 
       const shouldDeletePet = await Prompter.confirm({
         channel: message.channel,
-        question: areYouSureMessage,
+        question: messages.ABANDON_ARE_YOU_SURE,
         userId: call.caller,
       });
       if (shouldDeletePet !== true) {
         // ✅ ✖ -> user chose no.... KAFFE
-        this.send('**Phew.** Thank goodness you decided not to! Thank you for having a heart.');
+        this.send(messages.ABANDON_WILL_NOT_ABANDON_PET);
         return;
       }
 
       // eslint-disable-next-line
       await Pet.deleteOne({ _id: petToDelete._id }).exec();
       await Currency.add(call.caller, PET_ABANDON_RETURN_MONEY);
-      this.send(petAbandonedMessage.replace(/\{0\}/g, petToDelete.name));
+      this.send(messages.ABANDON_PET_ABANDONED.replace(/\{0\}/g, petToDelete.name));
       return;
     }
 
     // Arg === pat 💕
     if (args[0] && ['pat', 'pet', 'loveon', 'givelove', 'p'].includes(args[0].toLowerCase())) {
-      const petGotPatD = 'You have pat your pet **{0}** for **{1}** exp!';
-      await this.send(`You can pat your pets every **${PET_PAT_COOLDOWN}** minutes.`);
+      await this.send(messages.PAT_COOLDOWN);
       pets.forEach(pet => {
         const lastPatDate = pet.pats.time;
         if (Date.now() - lastPatDate.getTime() < 1800000) return;
@@ -194,7 +231,12 @@ module.exports = new Command({
           pet
             .givePat()
             .then(() => {
-              this.send(petGotPatD.replace(/\{0\}/g, pet.name).replace(/\{1\}/g, PET_PAT_EXP));
+              this.send(
+                messages.PAT_PATTED_THE_PET.replace(/\{0\}/g, pet.name).replace(
+                  /\{1\}/g,
+                  PET_PAT_EXP,
+                ),
+              );
             })
             .catch(() => {
               /* bruh */
@@ -210,33 +252,17 @@ module.exports = new Command({
       ['rename', 'changename', 'idk', 'helpmebluLOL', 'r'].includes(args[0].toLowerCase())
     ) {
       // Name Restrictions
-      // const characters = [14]ig
       const filter = new Filter();
 
-      // 1. check pet count
-      //    - if no pets, send message u got no pets u can adopt with blah blah....
-      const youHaveNoPetsMessage = "You don't have a pet to rename.";
-
       if (pets.length < 1) {
-        this.send(youHaveNoPetsMessage);
+        this.send(messages.RENAME_NO_PETS);
         return;
       }
 
-      // 2. check money
-      //    - if not enough, send message ur poor blah blah....
-      const insufficientMoney =
-        "Renaming costs {0}. You do not have enough funds to rename your pet. Please consider voting for me (**!b vote**) to gather more Blue Ink, or use **!b daily** if you haven't already!";
       if (call.profile.money < PET_RENAMING_PRICE) {
-        this.send(insufficientMoney.replace(/\{0\}/g, PET_RENAMING_PRICE));
+        this.send(messages.RENAME_NO_MONEY.replace(/\{0\}/g, PET_RENAMING_PRICE));
         return;
       }
-
-      // 2.25?? - which pet lol?
-      const whichPetToRename = 'Which pet would you like to rename? **{0}**?';
-
-      // this message is not needed since we are bestMatching
-      // const userEnteredAPetNameThatTheyDontOwnMessage =
-      //   "I do not see a pet under that name, maybe check to see that you are spelling it correctly?";
 
       let petToRename = null;
 
@@ -246,7 +272,7 @@ module.exports = new Command({
           // eslint-disable-next-line no-await-in-loop
           const res = await Prompter.message({
             channel: message.channel,
-            question: whichPetToRename.replace(/\{0\}/g, pets.map(p => p.name).join(', ')),
+            question: messages.RENAME_WHICH_PET.replace(/\{0\}/g, pets.map(p => p.name).join(', ')),
             userId: call.caller,
             max: 1,
             deleteMessage: false,
@@ -259,7 +285,6 @@ module.exports = new Command({
               .toLowerCase();
             petToRename = pets.find(p => p.name.toLowerCase() === chosenPetName);
             if (!petToRename) {
-              const variableNamesLMAO = 'I do not see a pet under this name; did you mean **{0}**?';
               const { bestMatch } = match(
                 chosenPetName,
                 pets.map(p => p.name),
@@ -268,7 +293,7 @@ module.exports = new Command({
               // eslint-disable-next-line no-await-in-loop
               const didWeGetIt = await Prompter.confirm({
                 channel: message.channel,
-                question: variableNamesLMAO.replace(/\{0\}/g, bestMatchName),
+                question: messages.RENAME_PET_NOT_FOUND.replace(/\{0\}/g, bestMatchName),
                 userId: call.caller,
               });
               if (didWeGetIt === true) {
@@ -283,39 +308,22 @@ module.exports = new Command({
         petToRename = pets[0];
       }
 
-      // 2.5? prompt confirm
-      //    - if no, send message okay then
-      const areYouSure = 'Are you sure you would like to rename your pet? That will cost **{0}**.';
-      const okayThen = 'Okie Dokie Artichokie.';
       // teehee okiedokieartichokie ~~~
       const response = await Prompter.confirm({
         channel: message.channel,
-        question: areYouSure.replace(/\{0\}/g, PET_RENAMING_PRICE),
+        question: messages.RENAME_ARE_YOU_SURE.replace(/\{0\}/g, PET_RENAMING_PRICE),
         userId: call.caller,
       });
 
       if (response !== true) {
-        this.send(okayThen);
+        this.send(messages.FANCY_OKAY);
         return;
       }
-
-      // 3. prompt for new name
-      //    - prompt for a name, check for profanity
-      //    - if profane, send message fk off dude blah blah....
-      //    - we'll use filter.isProfane();
-      const whatPetNewName = "What would you like your pet's new name to be?";
-      const okayThenV2 = 'Alrighty then.';
-      const nameIsProfane = `I'm going to need you to reconsider that name. That is an explicit name that I cannot allow you to name your pet that.`;
-      const nameHasWeirdStuff = 'Only characters A-Z are allowed in your pets name.';
-      const nameIsTooLong = `That name is too long. Pet names can only be ${PET_MAX_NAME_LENGTH} or less.`;
-      const userMoneyChangedDuringRenamingCannotAffordAnymore =
-        'You do not have enough Bloo ink to rename your pet.';
-      const successPetRenamed = `Your pet is now named {0} and you have {1} Bloo Ink.`;
 
       // ~~~oowoo~~~ 👍
       const responseList = await Prompter.message({
         channel: message.channel,
-        question: whatPetNewName,
+        question: messages.RENAME_ASK_NEW_NAME,
         userId: call.caller,
         deleteMessage: false,
       });
@@ -323,60 +331,43 @@ module.exports = new Command({
       const newName = responseList ? responseList.first().content.trim() : '';
 
       if (!newName) {
-        this.send(okayThenV2);
+        this.send(messages.SILLY_OKAY);
         return;
       }
 
       if (filter.isProfane(newName)) {
-        this.send(nameIsProfane);
+        this.send(messages.RENAME_NAME_HAS_PROFANITY);
         return;
       }
 
       if (/[^a-z ]/gi.test(newName)) {
-        this.send(nameHasWeirdStuff);
+        this.send(messages.RENAME_NAME_HAS_SYMBOLS);
         return;
       }
 
       if (newName > PET_MAX_NAME_LENGTH) {
-        this.send(nameIsTooLong);
+        this.send(messages.RENAME_NAME_TOO_LONG);
         return;
       }
 
-      // 4. rename
-      //    - change name
       petToRename.name = newName;
 
-      // 4.5 - final money check
       let balance = await Currency.getBalance(call.caller);
-      // steps:
-      // 5. save
-      //    - save to db
+
       try {
         if (balance < PET_RENAMING_PRICE) {
-          this.send(userMoneyChangedDuringRenamingCannotAffordAnymore);
+          this.send(messages.RENAME_BALANCE_CHANGED_DURING_RENAMING);
           return;
         }
         await petToRename.save();
       } catch {
-        this.send('Something went wrong. :c');
+        this.send(messages.SOMETHING_WENT_WRONG);
         return;
       }
 
-      // 5.5 take money from user
       balance = await Currency.subtract(call.caller, PET_PRICE);
 
-      // 6. send feedback message
-      //    - send message, success new pet name {petname}
-      this.send(successPetRenamed.replace(/\{0\}/g, newName).replace(/\{1\}/g, balance));
-      return;
-    }
-
-    // No pets
-    if (pets.length === 0) {
-      this.send(
-        "You don't have a pet yet. Would you like to adopt one?",
-        `Use **${this.client.options.prefix}pet adopt**`,
-      );
+      this.send(messages.RENAME_SUCCESS.replace(/\{0\}/g, newName).replace(/\{1\}/g, balance));
       return;
     }
 
@@ -407,33 +398,23 @@ module.exports = new Command({
       });
 
       const msg = [];
-      msg.push(`__Currently these are the best pets!.__`);
+      msg.push(messages.TOP_HEADING);
       top5.forEach(top5Pet => msg.push(top5Pet));
 
       this.send(...msg, { split: true });
       return;
     }
 
+    // No pets
+    if (pets.length === 0) {
+      this.send(
+        "You don't have a pet yet. Would you like to adopt one?",
+        `Use **${this.client.options.prefix}pet adopt**`,
+      );
+      return;
+    }
+
     // No args, show pets
-    pets.forEach(pet => {
-      const lastPatDate = pet.pats.time;
-      this.send({
-        embed: new MessageEmbed({
-          title: pet.name,
-          description: Text.lines(
-            `⭐ **Level:** __${pet.level}__`,
-            `✨ **Experience:** __${pet.experience}/${xp.expToNextLevel(pet.level)}__`,
-            `💕 **Pats:** __${pet.pats.count}__`,
-            Text.duration(
-              `**Last pat:** __{duration:${flatSeconds(
-                Date.now() - lastPatDate.getTime(),
-              )}}__ ago.`,
-            ),
-          ),
-          files: [{ name: 'pet.png', attachment: pet.image }],
-          thumbnail: { url: 'attachment://pet.png' },
-        }),
-      });
-    });
+    listPets(pets, message, args, call);
   },
 });
