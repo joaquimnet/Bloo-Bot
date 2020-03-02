fc60521eb30fd6f0b750547afd2246697998086e
Joaquim <joaquimmy@yahoo.com>
2020-03-01 22:34:14 -0300

    Formatting

--- src/commands/testing/listeners.js
+++ src/commands/testing/listeners.js
@@ -1,5 +1,7 @@
 const { Command } = require('chop-tools');
 
+const emoji = '<:right:652702479292825600> ';
+
 module.exports = new Command({
   name: 'listeners',
   description: 'Shows the loaded listeners.',
@@ -7,9 +9,10 @@ module.exports = new Command({
   aliases: ['list'],
   // delete: true,
   hidden: true,
-  run() {
-    const list = [...this.client.listeners.values()].map(l => l.toString());
+  async run() {
+    const list = [...this.client.listeners.values()].map(l => emoji + l.toString());
     // console.log(list);
-    this.send("```" + JSON.stringify(list, null, 2) + "```", true);
+    await this.send('These are the loaded listeners.');
+    this.send(...list, { split: true });
   },
 });
