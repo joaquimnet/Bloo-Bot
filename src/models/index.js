93463d4831f277e5be64ddbc9b152bfc5d5a058a
Joaquim <joaquimmy@yahoo.com>
2020-03-02 16:33:54 -0300

    Add index to models folder

---
+++ src/models/index.js
@@ -0,0 +1,17 @@
+const Bloo = require('./bloo');
+const GuildSettings = require('./guildSettings');
+const Idea = require('./idea');
+const Logs = require('./logs');
+const Missions = require('./missions');
+const Pet = require('./pet');
+const Profile = require('./profile');
+
+module.exports = {
+  Bloo,
+  GuildSettings,
+  Idea,
+  Logs,
+  Missions,
+  Pet,
+  Profile,
+};
