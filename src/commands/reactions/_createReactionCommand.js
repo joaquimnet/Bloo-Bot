ade6036666df15c8fc269216744d37cec782dcdf
Joaquim <joaquimmy@yahoo.com>
2020-03-04 20:11:45 -0300

    createReactionCommand wont crash if no msg

--- src/commands/reactions/_createReactionCommand.js
+++ src/commands/reactions/_createReactionCommand.js
@@ -3,16 +3,21 @@ const findPerson = require('../../util/findPerson');
 const Gifs = require('../../services/gifs');
 
 module.exports = options => {
-  return async function (message, args, call) {
+  return async function(message, args, call) {
     const target = await findPerson(message.mentions.members.first());
 
-    const msgTarget = options.msgTarget.replace(/%user/gi, `<@${call.caller}>`).replace(/%target/gi, target);
-    const msgNoTarget = options.msgNoTarget.replace(/%user/gi, `<@${call.caller}>`);
+    const msgTarget =
+      options.msgTarget &&
+      options.msgTarget.replace(/%user/gi, `<@${call.caller}>`).replace(/%target/gi, target);
+    const msgNoTarget =
+      options.msgNoTarget && options.msgNoTarget.replace(/%user/gi, `<@${call.caller}>`);
 
     const msg = target ? msgTarget : msgNoTarget;
+
+    if (!msg) return;
 
     const embed = makeEmbed(msg, await Gifs.random(options.gif), message);
 
     this.send({ embed });
-  }
-}
+  };
+};
